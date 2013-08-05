dc434daab63bc3598427b859634274cdba74c91c
Dave Richardson <richardsongdws@ornl.gov>
2013-08-05 16:02:53 -0400

    Updated yaml-config to also allow writing of configuration files with a new method writeConfig. Currently doing so will remove all comments in the configuration file, but otherwise appears to work well.
    Also updated the run.js file to ensure the changes to writeConfig are working.  Will update with tests soon

--- examples/run.js
+++ examples/run.js
@@ -1,18 +1,21 @@
 // $ cd examples; node run.js
-var config = require('..');
+var config = require('..')
+  , path = require('path');
 var env = process.env.NODE_ENV || 'development';
 
-var settings = config.readConfig('app.yaml'); // path from your app root without slash
+var settings = config.readConfig(path.join(__dirname,'app.yaml')); // path from your app root without slash
 console.log('env = %s', env);
 console.log(settings); // if NODE_ENV is development, prints 1
 
 env = 'test'
-settings = config.readConfig('app.yaml', env);
+settings = config.readConfig(path.join(__dirname,'app.yaml'),env);
 console.log('env = %s', env);
 console.log(settings);
 
 env = 'production'
-settings = config.readConfig('app.yaml', env);
+settings = config.readConfig(path.join(__dirname,'app.yaml'),env);
 console.log('env = %s', env);
 console.log(settings);
+settings.redis.host='10.1.50.100';
+config.writeConfig(settings, path.join(__dirname,'app.yaml'),env);
 
